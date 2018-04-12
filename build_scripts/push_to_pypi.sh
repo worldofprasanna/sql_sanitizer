da795b9a467f3594f5eaba425a8d9cd79f558750
Prasanna <vprasana@thoughtworks.com>
2018-04-12 10:49:55 +0530

    Create build_scripts

---
+++ build_scripts/push_to_pypi.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Set ~/.pypirc with this content
+#[distutils]
+#index-servers=pypi
+#
+#[pypi]
+#repository: https://pypi.python.org/pypi
+#username:worldofprasanna
+#password:<password>
+
+python setup.py sdist upload
